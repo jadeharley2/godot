ba7c294bf49caed67d9880793ec37e0a649fdcdf
jadeharley2 <gcalib9@gmail.com>
2025-10-25 13:45:52 +0300

    fog 2

--- servers/rendering/renderer_rd/shaders/forward_clustered/scene_forward_clustered.glsl
+++ servers/rendering/renderer_rd/shaders/forward_clustered/scene_forward_clustered.glsl
@@ -1093,8 +1093,12 @@ vec4 fog_process(vec3 vertex) {
 	if (scene_data_block.data.fog_aerial_perspective > 0.0) {
 		vec3 sky_fog_color = vec3(0.0);
 		vec3 cube_view = scene_data_block.data.radiance_inverse_xform * vertex;
-		// mip_level always reads from the second mipmap and higher so the fog is always slightly blurred
-		float mip_level = mix(1.0 / MAX_ROUGHNESS_LOD, 1.0, 1.0 - (abs(vertex.z) - scene_data_block.data.z_near) / (scene_data_block.data.z_far - scene_data_block.data.z_near));
+		// With depth fog, aerial perspective doesn't read mipmaps so the fog is not blurred
+		// (and is suitable for open world fog fading).
+		float mip_level = 0.0;
+		if (!sc_use_depth_fog()) {
+			mip_level = mix(1.0 / MAX_ROUGHNESS_LOD, 1.0, 1.0 - (abs(vertex.z) - scene_data_block.data.z_near) / (scene_data_block.data.z_far - scene_data_block.data.z_near));
+		}
 #ifdef USE_RADIANCE_OCTMAP_ARRAY
 		float roughness_lod, blend;
 		blend = modf(mip_level * MAX_ROUGHNESS_LOD, roughness_lod);
