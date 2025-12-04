8936cddbc5bb6f2ce1a1cc0a0b9c9b6c1317e7df
jadeharley2 <gcalib9@gmail.com>
2025-12-05 00:59:14 +0300

    template copy bat

---
+++ _copy_template.bat
@@ -0,0 +1,2 @@
+copy "bin\godot.windows.template_debug.x86_64.exe" "bin\windows_debug_x86_64.exe" /Y
+copy "bin\godot.windows.template_debug.x86_64.console.exe" "bin\windows_debug_x86_64.console.exe" /Y
