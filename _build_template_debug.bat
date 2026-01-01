802497fe4c42cd332307f23bdefe642e86c90334
jadeharley2 <gcalib9@gmail.com>
2026-01-01 14:31:06 +0300

    add build commands

---
+++ _build_template_debug.bat
@@ -0,0 +1,3 @@
+scons target=template_debug debug_symbols=true
+copy "bin\godot.windows.template_debug.x86_64.exe" "bin\windows_debug_x86_64.exe" /Y
+copy "bin\godot.windows.template_debug.x86_64.console.exe" "bin\windows_debug_x86_64.console.exe" /Y
