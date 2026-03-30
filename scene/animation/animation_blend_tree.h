87a65c3b19fc5a6d85ca22de5cf76f2275cfc182
jadeharley2 <gcalib9@gmail.com>
2026-03-31 02:40:10 +0300

    add `await_end` to `AnimationNodeTransition`

--- scene/animation/animation_blend_tree.h
+++ scene/animation/animation_blend_tree.h
@@ -336,6 +336,7 @@ class AnimationNodeTransition : public AnimationNodeSync {
 		bool auto_advance = false;
 		bool break_loop_at_end = false;
 		bool reset = true;
+		bool await_end = false;
 	};
 	LocalVector<InputData> input_data;
 
@@ -387,6 +388,9 @@ public:
 
 	void set_allow_transition_to_self(bool p_enable);
 	bool is_allow_transition_to_self() const;
+	
+	void set_await_end(int p_input, bool p_enable);
+	bool get_await_end(int p_input) const;
 
 	virtual NodeTimeInfo _process(ProcessState &p_process_state, AnimationNodeInstance &p_instance, const AnimationMixer::PlaybackInfo &p_playback_info, bool p_test_only = false) override;
 
